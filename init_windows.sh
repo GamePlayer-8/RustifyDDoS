a011eec0f3f7eb4e0f831173f45977cff76a4940
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-05-01 15:14:55 +0200

    Fix Windows EXE Builder.

--- init_windows.sh
+++ init_windows.sh
@@ -16,14 +16,21 @@ markdown README.md >> index.html
 echo '</body>' >> index.html
 echo '</html>' >> index.html
 
-apt install --yes rust cargo git apt-utils build-essential curl tar wget xvfb > /dev/null
+apt install --yes gcc-mingw-w64 pkg-config libssl-dev rustc cargo git apt-utils build-essential curl tar wget xvfb > /dev/null
+
+export RUSTUP_INIT_SKIP_PATH_CHECK=yes
+curl https://sh.rustup.rs > rustup.rs
+sh rustup.rs -y
+
+rm -f rustup.rs
 
 Xvfb -ac :0 -screen 0 1280x1024x24 &
 sleep 5
 
+source "$HOME/.cargo/env"
 rustup target add x86_64-pc-windows-gnu
 cargo build --release --target x86_64-pc-windows-gnu
-mv target/release/rddos.exe .
+mv target/x86_64-pc-windows-gnu/release/rddos.exe .
 rm -rf target
 
 chmod +x rddos.exe
